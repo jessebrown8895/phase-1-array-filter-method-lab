d358af3a9484e328a07307221c59cb73d1123272
Jesse Brown <Brownjesse8895@yahoo.com>
2021-06-03 13:44:30 -0500

    Using the Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(arrayNames, string){
+    let names = arrayNames.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return names
+}
+
+function  fuzzyMatch(arrayNames, string){
+    let names = arrayNames.filter(driver => driver[0] === string[0])
+    
+    return names
+}
+
+function matchName(arrayNames, string){
+    let names = arrayNames.filter(driver => driver.name === string)
+    return names  
+}
+
