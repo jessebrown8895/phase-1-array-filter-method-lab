f1de7f788f5be8426c37cba06a7aa547e409f41d
Jesse Brown <Brownjesse8895@yahoo.com>
2021-10-11 12:44:43 -0500

    Using the Array Filter Method
    In this lab we created three arrow functions the first first functions takes in an array of driver names and a string as an argument and filters through the drivers and returns a matching list of drivers and whos names are lowercase. The second function takes in an array of drivers and a string as an argument and returns all drivers whos names begin with the provide first letter. The last arrow function takes in an array of driver objects and a string as an arugment and returns each element whos name property matches the provided string argument

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-function findMatching(arrayNames, string){
-    let names = arrayNames.filter(driver => driver.toLowerCase() === string.toLowerCase())
-    return names
+// function findMatching(arrayNames, string){
+//     let names = arrayNames.filter(driver => driver.toLowerCase() === string.toLowerCase())
+//     return names
+// }
+const findMatching= (arrayNames, string) => {
+    return arrayNames.filter(driver => driver.toLowerCase() === string.toLowerCase())
 }
 
-function  fuzzyMatch(arrayNames, string){
-    let names = arrayNames.filter(driver => driver[0] === string[0])
+const fuzzyMatch = (arrayDrivers, string) => {
+   return arrayDrivers.filter(drivers => drivers[0] === string[0])
     
-    return names
 }
 
-function matchName(arrayNames, string){
-    let names = arrayNames.filter(driver => driver.name === string)
-    return names  
+const matchName = (arrayDrivers, string) => {
+    return arrayDrivers.filter(drivers => drivers.name === string)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function  fuzzyMatch(arrayNames, string){
+//     let names = arrayNames.filter(driver => driver[0] === string[0])
+    
+//     return names
+// }
+
+// function matchName(arrayNames, string){
+//     let names = arrayNames.filter(driver => driver.name === string)
+//     return names  
+// }
 
